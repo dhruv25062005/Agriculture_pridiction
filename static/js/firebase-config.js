19947b1c9e553daefef3de30ab24cffd4912ee86
Dhruv Gupta <155224274+dhruv25062005@users.noreply.github.com>
2026-09-11 20:10:17 +0530

    feat: load crop field auth background

--- static/js/firebase-config.js
+++ static/js/firebase-config.js
@@ -59,4 +59,5 @@ export{app,auth,db,doc,getDocFromServer,setDoc,getDoc,getDocs,collection,query,o
 if(typeof document!=="undefined"){
   import("/static/js/auth-3d.js?v=20260911-auth3d1").catch(error=>console.debug("Auth visual scene unavailable:",error));
   import("/static/js/auth-ui-upgrade.js?v=20260911-authui1").catch(error=>console.debug("Auth UI layer unavailable:",error));
+  import("/static/js/auth-background.js?v=20260911-fieldbg1").catch(error=>console.debug("Auth background unavailable:",error));
 }
